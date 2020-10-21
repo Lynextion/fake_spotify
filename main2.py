a1ffc61db70b0566f73be6e7c20a3b180c248718
Lynextion <47065577+Lynextion@users.noreply.github.com>
2020-10-21 17:18:40 +0300

    Done

--- main2.py
+++ main2.py
@@ -10,69 +10,131 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pytube import YouTube
-
+import time
+import os
+import re
+import string
+import shutil
 
 
 class player():
     def __init__(self):
         self.driver=webdriver.Chrome()
-        self.wait = WebDriverWait(driver,3)
+        self.wait = WebDriverWait(self.driver,3)
         self.presence = EC.presence_of_element_located
         self.visible = EC.visibility_of_element_located
         self.music_title = []
         self.singer = []
         self.music_href = []
         self.img_url = []
+        self.installed_music = []
+        self.music_id = ""
+        self.counter =0
+        self.id ='a'
 
-
-    def download(self,url):
-        self.yt = YouTube(url)
-        self.stream = yt.streams.filter(only_audio=True).first()
-        self.stream.download()
-        self.url
-        self.bf
-
-    def chrome(self)
-        while True:
-                
-            self.search(    
-            for section in bf.find_all('div', class_='style-scope ytd-video-renderer')  
-                try:
-                    title=section.find('h3', class_='title-and-badge style-scope ytd-video-renderer').text
-                    print(title)
-                    self.music_title.append(title   
-                    sing = section.find('a', class_='yt-simple-endpoint style-scope yt-formatted-string').text
-                    print(sing)
-                    self.singer.append(sing 
-                    href = section.find('a', class_='yt-simple-endpoint style-scope ytd-video-renderer', href=True)
-                    self.music_href.append(href['href'] 
-                    img = section.find('img')
-                    self.img_url.append(img.get('src')  
-                except AttributeError:
-                    continue
-                
-           
-
-    def music_choose(self):
-
-        music_choice = int(input('Please enter num for choose:'))
+        self.current_dic = os.getcwd()
+        self.music_file = os.path.join(self.current_dic,'')
+        directory = 'musics'
+        self.path = os.path.join(self.current_dic, directory)
+        
         try:
-
-            self.img_data = requests.get(self.img_url[music_choice]).content  
-
-            with open('image.jpg','wb') as f:
-                f.write(img_data)
+            os.mkdir(path)
         except:
-            continue
+            print('ok')
 
-        self.url = "https://www.youtube.com"+self.music_href[music_choice] 
-        self.driver.get(url)  
-        print("Downloading please wait...")
-        self.download(url)
-        print("Download complete")
+        
 
+
+    def download(self,url,music_title):
+        
+        self.yt = YouTube(url)
+        self.stream = self.yt.streams.filter(only_audio=True).first()
+        out_file = self.stream.download()
+        time.sleep(0.8)
+        music_place = out_file
+        music_place = music_place.replace(self.music_file,'')
+        re.sub('\ |\?|\.|\!|\/|\;|\:', '',music_title)
+        
+        os.rename(out_file,self.path+"\\"+music_title)
+        
+
+        return (self.path+"\\"+music_title)
+        
+        
+
+                
+
+    def chrome(self):
+        time.sleep(0.8)
+        self.music_title = []
+        self.music_href = []
+        self.bf = BeautifulSoup(self.driver.page_source,'lxml')     
+          
+        for section in self.bf.find_all('ytd-video-renderer', class_='style-scope ytd-item-section-renderer'):  
+            try:
+                title=section.find('h3', class_='title-and-badge style-scope ytd-video-renderer').text
+                print(title)
+                self.music_title.append(title)   
+                sing = section.find('a', class_='yt-simple-endpoint style-scope yt-formatted-string').text
+                print(sing)
+                self.singer.append(sing) 
+                href = section.find('a', class_='yt-simple-endpoint style-scope ytd-video-renderer', href=True)
+                self.music_href.append(href['href']) 
+                img = section.find('img')
+                self.img_url.append(img.get('src'))  
+
+            except AttributeError:
+                continue
+
+        return  self.music_title,self.music_href
+                
+        
+
+    def music_choose(self,music,music_title):
+        re.sub('\ |\?|\.|\!|\/|\;|\:', '',music_title)
+        
+        # try:
+
+         #    self.img_data = requests.get(self.img_url[music_choice]).content  
+
+          #   with open('image.jpg','wb') as f:
+           #      f.write(img_data)
+         #except:
+          #   print("nah")
+  
+        
+
+        self.musics = open('names.txt','r')
+
+        find = 0
+        lists = self.musics.readlines()
+        for line in lists:
+            print(line,music_title)
+            if line == music_title:
+                print(line,"mmmmmm",music_title)
+                find = 1
+                name= music_title.replace(self.music_file,'')
+                return (self.path+'\\'+name)
+                pass
+                       
+
+        if find == 0:
+            
+            self.musics.close()
+            self.musics = open('names.txt','a')
+            self.url = "https://www.youtube.com"+music
+            print("url",self.url)
+            self.driver.get(self.url)  
+            print("Downloading please wait...")
+            name = self.download(self.url,music_title)
+            self.musics.write(music_title+'\n')
+            self.musics.close()
+            print("Download complete")
+
+            return (name)
+        find = 0
+                
 
     def search(self,choice):
         self.url = 'https://www.youtube.com/results?search_query='+choice
-        self.driver.get(url)
-        self.bf = BeautifulSoup(driver.page_source,'lxml')
+        self.driver.get(self.url)
--- window.py
+++ window.py
@@ -1,30 +1,84 @@
 
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets 
+from PyQt5.QtWidgets import (QListWidget, QWidget, QMessageBox, 
+    QApplication, QVBoxLayout)
+
+from PyQt5.QtGui import QIcon 
+from PyQt5 import QtMultimedia as M
+import threading
+import time
+import os
+
+
 import main2
 
 class Ui_MainWindow(object):
-    def __init(self):
-        player = main2.player()
+    def __init__(self):
+        self.player = main2.player()
+        self.music_titles = []
+        self.music_href = []
+        self.index = ""
+        self.music_player = M.QMediaPlayer()
+        self.searched = ""
+        
 
     def setupUi(self, MainWindow):
+        os.environ['QT_MULTIMEDIA_PREFERRED_PLUGINS'] = 'windowsmediafoundation'
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.img = QtWidgets.QLabel(self.centralwidget)
-        self.img.setGeometry(QtCore.QRect(100, 90, 511, 251))
-        self.img.setObjectName("img")
+       # self.img = QtWidgets.QLabel(self.centralwidget)
+        #self.img.setGeometry(QtCore.QRect(100, 90, 511, 251))
+        #self.img.setObjectName("img")
+
+
+
+
+        self.currentTimeLabel = QtWidgets.QLabel(self.centralwidget)
+        self.currentTimeLabel.setMinimumSize(QtCore.QSize(80, 0))
+        self.currentTimeLabel.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTrailing|QtCore.Qt.AlignVCenter)
+        self.currentTimeLabel.setObjectName("currentTimeLabel")
+        self.currentTimeLabel.setGeometry(QtCore.QRect(70, 325, 61, 90))
+
+        self.volumeSlider = QtWidgets.QSlider(self.centralwidget)
+        self.volumeSlider.setMaximum(100)
+        self.volumeSlider.setProperty("value", 100)
+        self.volumeSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.volumeSlider.setObjectName("volumeSlider")
+        self.volumeSlider.setGeometry(QtCore.QRect(550, 440, 61, 22))
+
+        self.timeSlider = QtWidgets.QSlider(self.centralwidget)
+        self.timeSlider.setOrientation(QtCore.Qt.Horizontal)
+        self.timeSlider.setObjectName("timeSlider")
+        self.timeSlider.setGeometry(QtCore.QRect(160, 360, 481, 22))
+        self.timeSlider.setOrientation(QtCore.Qt.Horizontal)
+
+
+        self.totalTimeLabel = QtWidgets.QLabel(self.centralwidget)
+        self.totalTimeLabel.setMinimumSize(QtCore.QSize(80, 0))
+        self.totalTimeLabel.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.totalTimeLabel.setGeometry(QtCore.QRect(650,325,61,90))
+        self.timeSlider.setOrientation(QtCore.Qt.Horizontal)
+
+        self.list=QListWidget(self.centralwidget)
+        self.list.setGeometry(QtCore.QRect(100, 90, 511, 251))
         self.Play = QtWidgets.QPushButton(self.centralwidget)
         self.Play.setGeometry(QtCore.QRect(350, 440, 61, 41))
         self.Play.setObjectName("Play")
+        self.Play.clicked.connect(self.Start)
+        self.pause_bt = QtWidgets.QPushButton(self.centralwidget)
+        self.pause_bt.setGeometry(QtCore.QRect(420,440,61,41))
+        self.pause_bt.setObjectName('Pause')
+        self.pause_bt.clicked.connect(self.Pause)
         self.Next = QtWidgets.QPushButton(self.centralwidget)
         self.Next.setGeometry(QtCore.QRect(310, 450, 31, 31))
         self.Next.setObjectName("Next")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(156, 390, 421, 31))
-        self.label_2.setObjectName("label_2")
+        self.label_title = QtWidgets.QLabel(self.centralwidget)
+        self.label_title.setGeometry(QtCore.QRect(156, 390, 421, 31))
+        self.label_title.setObjectName("label_title")
         self.Search = QtWidgets.QLineEdit(self.centralwidget)
-        self.Search.setGeometry(QtCore.QRect(192, 20, 411, 20))
+        self.Search.setGeometry(QtCore.QRect(172, 20, 411, 20))
         self.Search.setText("")
         self.Search.setObjectName("Search")
         self.Search_button = QtWidgets.QPushButton(self.centralwidget)
@@ -38,25 +92,101 @@ class Ui_MainWindow(object):
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
+        #self.horizontalSlider = QtWidgets.QSlider(self.centralwidget)
+        #self.horizontalSlider.setGeometry(QtCore.QRect(160, 360, 481, 22))
+        #self.horizontalSlider.setOrientation(QtCore.Qt.Horizontal)
+        #self.horizontalSlider.setObjectName("horizontalSlider")
+    
+        
+
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         
         self.Search_button.clicked.connect(self.search_clicked)
+        self.list.clicked.connect(self.list_OnClicked)
 
     def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.img.setText(_translate("MainWindow", "TextLabel"))
-        self.Play.setText(_translate("MainWindow", "Play"))
-        self.Next.setText(_translate("MainWindow", "Next"))
-        self.label_2.setText(_translate("MainWindow", "TextLabel"))
-        self.Search_button.setText(_translate("MainWindow", "Search"))
+        self._translate = QtCore.QCoreApplication.translate
+        self.currentTimeLabel.setText(self._translate("MainWindow", "0:00"))
+        MainWindow.setWindowTitle(self._translate("MainWindow", "Fake Spotify"))
+        #self.img.setText(self._translate("MainWindow", "TextLabel"))
+        self.Play.setText(self._translate("MainWindow", "Play"))
+        self.Next.setText(self._translate("MainWindow", "Next"))
+        self.pause_bt.setText(self._translate("MainWindow","Pause"))
+        self.Search_button.setText(self._translate("MainWindow", "Search"))
+        self.label_title.setText(self._translate("MainWindow", "Download complete"))
+        self.music_player.durationChanged.connect(self.update_duration)
+        self.music_player.positionChanged.connect(self.update_position)
+        self.timeSlider.valueChanged.connect(self.music_player.setPosition)
+        self.volumeSlider.valueChanged.connect(self.music_player.setVolume)
     
+    def Start(self):
+        self.music_player.play()
+    def Pause(self):
+        self.music_player.pause()
+
+    #ilk bu çalışacak
     def search_clicked(self):
-      searched =  self.Search.text()
-      player.search(searched)
-      
+        self.searched =  self.Search.text()
+        self.thread =threading.Thread(target=self.player.search, args = (self.searched,))
+        self.thread.start()
+        self.insert_list()
+
+    #buna basıldığında şarkı açılması lazım
+    def insert_list(self):
+        self.list.clear()
+        self.music_titles,self.music_href = self.player.chrome()
+        cache = ""
+    
+         
+        for title in self.music_titles:
+             title = title.strip()
+    
+             if cache != title:
+                 self.list.addItem(title)
+             cache = title
+    
+    #when user click the  listwiew
+    def list_OnClicked(self):
+        self.thread = threading.Thread(target = self.play_music)
+        self.thread.start()
+
+
+    def set_label_title(self):
+        self.label_title.setText(self._translate("MainWindow",self.music_titles[self.index]))
+        
+        #play music thread
+    def play_music(self):
+        self.index = self.list.row(self.list.currentItem())
+        print(self.music_href[self.index],"index",self.index)
+        name = self.player.music_choose(self.music_href[self.index],self.music_titles[self.index].strip() + ".mp4")
+        self.url = QtCore.QUrl.fromLocalFile(name)
+        self.content = M.QMediaContent(self.url)
+        self.music_player.setMedia(self.content)
+        self.music_player.play()
+
+    def update_duration(self,duration):
+        print("!", duration)
+        print("?", self.music_player.duration())
+ 
+        self.timeSlider.setMaximum(duration)
+        if duration >= 0:
+            self.totalTimeLabel.setText(self.hhmmss(duration))
+
+    def update_position(self, position):
+        if position >= 0:
+            self.currentTimeLabel.setText(self.hhmmss(position))
+
+        self.timeSlider.blockSignals(True)
+        self.timeSlider.setValue(position)
+        self.timeSlider.blockSignals(False)
+    
+    def hhmmss(self,ms):
+        s = round(ms / 1000)
+        m,s = divmod(s,60)
+        h,m = divmod(m,60)
+        return ("%d:%02d:%02d" % (h,m,s)) if h else ("%d:%02d" % (m,s))
 
 if __name__ == "__main__":
     import sys
